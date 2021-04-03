9fc45a850a457236e05f412503bb6f2da05ae5ce
Mohammad Uddin <mtaslimu010@gmail.com>
2021-04-03 12:13:32 -0400

    WebAutomationFrameworkProject-2

---
+++ Amazon-2/src/test/java/landingpage/PageTitle.java
@@ -0,0 +1,18 @@
+package landingpage;
+
+import Base.CommonAPI;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PageTitle extends CommonAPI {
+
+    @Test
+    public void checkTitle(){
+        String expectedTitle = "Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more";
+        String actualTitle = driver.getTitle();
+        System.out.println(actualTitle);
+        Assert.assertEquals(actualTitle, expectedTitle);
+
+    }
+
+}
